10ab4c0d959e698e2bb1fb53ca31116770eba7f8
Thomas Schuehly <thomas.schuehly@mimacom.com>
2023-03-10 21:16:24 +0100

    fix: v0.3.1: @ViewComponent with 2+ parameters

--- src/main/kotlin/com/example/thymeleafcomponentdemo/web/parameter/ParameterViewComponent.kt
+++ src/main/kotlin/com/example/thymeleafcomponentdemo/web/parameter/ParameterViewComponent.kt
@@ -1,13 +1,17 @@
 package com.example.thymeleafcomponentdemo.web.parameter
 
+import com.example.thymeleafcomponentdemo.core.ExampleService
 import de.tschuehly.thymeleafviewcomponent.ViewComponent
 import de.tschuehly.thymeleafviewcomponent.ViewContext
 import de.tschuehly.thymeleafviewcomponent.toProperty
 
 
 @ViewComponent
-class ParameterViewComponent {
-    fun render(parameter: String?) = ViewContext(
-            "office" toProperty (parameter ?: throw Exception("You need to pass in a parameter")),
+class ParameterViewComponent(
+    private val exampleService: ExampleService
+) {
+    fun render(parameter: String?, parameter2: String?) = ViewContext(
+        "testparameter" toProperty (parameter ?: throw Exception("You need to pass in a parameter")),
+        "office" toProperty (parameter2 ?: exampleService.getOfficeHours())
     )
 }
