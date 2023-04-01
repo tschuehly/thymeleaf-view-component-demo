e0f695da5a6915232646a024c2eb2fccd0f173f6
Thomas Schuehly <thomas.schuehly@outlook.com>
2023-04-01 18:14:55 +0200

    feature: v0.4.1 ViewContextContainer to compose pages from multiple components -> support of out of band response

--- src/main/kotlin/com/example/thymeleafcomponentdemo/web/Router.kt
+++ src/main/kotlin/com/example/thymeleafcomponentdemo/web/Router.kt
@@ -1,16 +1,27 @@
 package com.example.thymeleafcomponentdemo.web
 
 import com.example.thymeleafcomponentdemo.web.home.HomeViewComponent
+import com.example.thymeleafcomponentdemo.web.navigation.NavigationViewComponent
 import de.tschuehly.thymeleafviewcomponent.ViewContext
+import de.tschuehly.thymeleafviewcomponent.ViewContextContainer
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class Router(
-    val homeViewComponent: HomeViewComponent
+    val homeViewComponent: HomeViewComponent,
+    private val navigationViewComponent: NavigationViewComponent
 ) {
     @GetMapping( "/")
     fun homeComponent(): ViewContext {
         return homeViewComponent.render()
+    }
+
+    @GetMapping("/oob-test")
+    fun multipleComponent(): ViewContextContainer{
+        return ViewContextContainer(
+            navigationViewComponent.render(),
+            homeViewComponent.render()
+        )
     }
 }
